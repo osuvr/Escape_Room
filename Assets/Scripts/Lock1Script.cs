1de4bdd383a7f32a1966edc118c5601611b0eb64
OSUvislab <nmcvislab@gmail.com>
2019-02-20 20:52:11 -0800

    pickup and throw working

---
+++ Assets/Scripts/Lock1Script.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lock1Script : MonoBehaviour {
+
+    public static bool havekey1;
+    public bool inTrigger;
+
+    void OnTriggerEnter(Collider other)
+    {
+        inTrigger = true;
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        inTrigger = false;
+    }
+
+    void Update()
+    {
+        if (inTrigger && havekey1)
+        {
+            Destroy(this.gameObject);
+            DoorScript.doorKey1 = true;
+        }
+    }
+
+    void OnGUI()
+    {
+        if (inTrigger)
+        {
+            GUI.Box(new Rect(0, 0, 200, 25), "Lock 1 is unlocked");
+        }
+    }
+}
