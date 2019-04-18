be8c34b28177a8484552a94c42168db628cdd8a2
OSUvislab <nmcvislab@gmail.com>
2019-04-17 20:59:37 -0700

    Added lock script

---
+++ Assets/Lock.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lock : MonoBehaviour
+{
+    public GameObject Key;
+    private Collider keyCollider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        keyCollider = Key.GetComponent<Collider>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.GetInstanceID() == Key.GetInstanceID())
+        {
+            Debug.Log("Key entered");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
