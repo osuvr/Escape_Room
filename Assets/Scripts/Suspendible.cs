0b82189e12a8fc123bd6686b069f83bf23317298
OSUvislab <nmcvislab@gmail.com>
2019-05-22 20:47:23 -0700

    Added painting and new house model

---
+++ Assets/Scripts/Suspendible.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Suspendible : MonoBehaviour
+{
+    private Rigidbody rigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    public void Suspend()
+    {
+        rigidbody.isKinematic = true;
+    }
+
+    public void Unsuspend()
+    {
+        rigidbody.isKinematic = false;
+    }
+}
